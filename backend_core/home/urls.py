39b2ac689cc2f7723b86cc93b615fa56f657e525
Maciej Piszczek <maciej.piszczek93@gmail.com>
2022-10-19 23:56:12 +0200

    partialized templates

--- backend_core/home/urls.py
+++ backend_core/home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.LandingPageView.as_view(), name='index'),
+    path('add_donation/', views.AddDonationView.as_view(), name='add-donation'),
 ]
--- backend_core/home/views.py
+++ backend_core/home/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class LandingPageView(TemplateView):
     template_name = 'index.html'
+
+
+class AddDonationView(TemplateView):
+    template_name = 'form.html'
