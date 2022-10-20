fe93e9d1cf345f54a5dfa04720e934e86a72615a
Maciej Piszczek <maciej.piszczek93@gmail.com>
2022-10-20 15:15:25 +0200

    added Institution model

--- backend_core/home/models.py
+++ backend_core/home/models.py
@@ -6,3 +6,18 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Institution(models.Model):
+    class InstitutionTypes(models.TextChoices):
+        FN = "1", "foundation"
+        NGO = "2", "non-governmental organisation"
+        FR = "3", "local fundraising"
+
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    type = models.CharField(max_length=200, choices=InstitutionTypes.choices, default=InstitutionTypes.FN)
+    categories = models.ManyToManyField('Category', related_name='institutions')
+
+    def __str__(self):
+        return self.name
