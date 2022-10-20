38fcb6bdb187f8a06c6627c031d5ff443117b308
Maciej Piszczek <maciej.piszczek93@gmail.com>
2022-10-20 23:13:27 +0200

    amount of donated bags displayed properly

--- backend_core/home/views.py
+++ backend_core/home/views.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from django.views.generic import TemplateView
 
 from home.models import Donation, Institution
@@ -8,8 +10,14 @@ class LandingPageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['donations_count'] = Donation.objects.count()
+
+        context['bags_count'] = sum([donation.quantity for donation in Donation.objects.all()])
         context['inst_count'] = Institution.objects.count()
+
+        foundations = Institution.objects.filter(type="1")
+        ngos = Institution.objects.filter(type="2")
+        local_fundrs = Institution.objects.filter(type="3")
+        context['institutions'] = [(1, foundations), (2, ngos), (3, local_fundrs)]
 
         return context
 
