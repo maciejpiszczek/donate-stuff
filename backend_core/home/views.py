fca88d5ea76e545456739c8a4ba58b71f96dce8d
Maciej Piszczek <maciej.piszczek93@gmail.com>
2022-10-20 19:15:43 +0200

    added dynamic stats - number of supported institutions and donations

--- backend_core/home/views.py
+++ backend_core/home/views.py
@@ -1,8 +1,17 @@
 from django.views.generic import TemplateView
 
+from home.models import Donation, Institution
+
 
 class LandingPageView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['donations_count'] = Donation.objects.count()
+        context['inst_count'] = Institution.objects.count()
+
+        return context
 
 
 class AddDonationView(TemplateView):
