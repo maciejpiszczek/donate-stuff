4060bd1c5db827d1d6d216342ffdcdbfe5b3060d
Maciej Piszczek <maciej.piszczek93@gmail.com>
2023-01-22 12:52:56 +0100

    fixed data serialization after filling the donation form

--- backend_core/static/js/app.js
+++ backend_core/static/js/app.js
@@ -335,14 +335,14 @@
       let formData = new FormData(form);
       const csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 
-      function sendJson(inData) {
+      function sendJson() {
         const quantity = document.getElementById("quantity").value;
         const institution = document.querySelector('input[name="institution"]:checked').nextElementSibling.nextElementSibling.firstElementChild.firstElementChild.firstElementChild.value;
         const chosenCats = document.querySelectorAll('input[name="categories"]:checked');
         let chosenCatsIds = [];
 
         chosenCats.forEach(cat => {
-          const catId = cat.nextElementSibling.nextElementSibling.firstElementChild.firstElementChild.value;
+          const catId = cat.nextElementSibling.nextElementSibling.firstElementChild.value;
           chosenCatsIds.push(catId);
         })
 
@@ -373,10 +373,11 @@
           "pick_up_time": pickUpTime,
           "pick_up_comment": pickUpComment
         })
+        console.log(data);
         xhr.send(data);
       }
 
-      sendJson(formData);
+      sendJson();
 
       setTimeout(function() {
         document.location.href = "/";
